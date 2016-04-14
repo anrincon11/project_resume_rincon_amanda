e20242696e01cbf2b036f070092ca8279dc2e6bd
Amanda Rincon <anrincon@ufl.edu>
2016-04-14 16:10:21 -0700

    adjust animate scroll code and add ScrollUp plugin

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// ANIMATE SCROLL PLUGIN //
+
+$( '#one' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#education' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+$( '#two' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#experience' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+$( '#three' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#skills' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+$( '#four' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#interests' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+$( '#five' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#achievements' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+$( '#six' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+	$( '#contact' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+} );
+
+// SCROLL UP PLUGIN //
+
+$( function() {
+	$.scrollUp( {
+		scrollName: 'scrollUp', // Element ID
+		topDistance: '300', // Distance from top before showing element (px)
+		topSpeed: 300, // Speed back to top (ms)
+		animation: 'fade', // Fade, slide, none
+		animationInSpeed: 200, // Animation in speed (ms)
+		animationOutSpeed: 200, // Animation out speed (ms)
+		scrollText: 'Scroll to top', // Text for element
+		activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	} );
+} );
