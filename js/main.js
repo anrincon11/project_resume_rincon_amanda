da2f5a8c3a99869a148386a8605a2684e4c5dc3b
Amanda Rincon <anrincon@ufl.edu>
2016-04-15 16:27:58 -0700

    add jQuery document.ready function to js file

--- js/main.js
+++ js/main.js
@@ -1,170 +1,174 @@
-// ANIMATE SCROLL PLUGIN //
+$( document ).ready( function() {
 
-$( '#one' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#education' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
+	// ANIMATE SCROLL PLUGIN //
 
-$( '#two' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#experience' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
-
-$( '#three' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#skills' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
-
-$( '#four' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#interests' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
-
-$( '#five' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#achievements' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
-
-$( '#six' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
-	$( '#contact' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
-} );
-
-// SCROLL UP PLUGIN //
-
-$( function() {
-	$.scrollUp( {
-		scrollName: 'scrollUp', // Element ID
-		topDistance: '200', // Distance from top before showing element (px)
-		topSpeed: 800, // Speed back to top (ms)
-		animation: 'fade', // Fade, slide, none
-		animationInSpeed: 500, // Animation in speed (ms)
-		animationOutSpeed: 100, // Animation out speed (ms)
-		scrollText: 'Scroll to top', // Text for element
-		activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	$( '#one' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#education' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
 	} );
-} );
 
-// HOVER EFFECT - CUSTOM JQUERY CODE //
+	$( '#two' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#experience' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+	} );
 
-// ---------- DESIGN ICON ---------- //
+	$( '#three' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#skills' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+	} );
 
-$( '#design' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS DESIGN INTEREST
-	$( '#design img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#four' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#interests' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+	} );
 
-$( '#design' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES DESIGN INTEREST
-	$( '#design img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '35%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#five' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#achievements' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+	} );
 
-// ---------- MUSIC ICON ---------- //
+	$( '#six' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
+		$( '#contact' ).animatescroll(); //SCROLL WITH ANIMATION TO SECTION
+	} );
 
-$( '#music' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS MUSIC INTEREST
-	$( '#music img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// SCROLL UP PLUGIN //
 
-$( '#music' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES MUSIC INTEREST
-	$( '#music img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '35%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( function() {
+		$.scrollUp( {
+			scrollName: 'scrollUp', // Element ID
+			topDistance: '200', // Distance from top before showing element (px)
+			topSpeed: 800, // Speed back to top (ms)
+			animation: 'fade', // Fade, slide, none
+			animationInSpeed: 500, // Animation in speed (ms)
+			animationOutSpeed: 100, // Animation out speed (ms)
+			scrollText: 'Scroll to top', // Text for element
+			activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+		} );
+	} );
 
-// ---------- EXERCISING ICON ---------- //
+	// HOVER EFFECT - CUSTOM JQUERY CODE //
 
-$( '#exercising' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS EXERCISING INTEREST
-	$( '#exercising img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '100%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- DESIGN ICON ---------- //
 
-$( '#exercising' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES EXERCISING INTEREST
-	$( '#exercising img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#design' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS DESIGN INTEREST
+		$( '#design img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-// ---------- TRAVELING ICON ---------- //
+	$( '#design' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES DESIGN INTEREST
+		$( '#design img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '35%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-$( '#traveling' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS TRAVELING INTEREST
-	$( '#traveling img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- MUSIC ICON ---------- //
 
-$( '#traveling' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES TRAVELING INTEREST
-	$( '#traveling img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#music' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS MUSIC INTEREST
+		$( '#music img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-// ---------- PAINTING ICON ---------- //
+	$( '#music' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES MUSIC INTEREST
+		$( '#music img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '35%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-$( '#painting' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS PAINTING INTEREST
-	$( '#painting img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- EXERCISING ICON ---------- //
 
-$( '#painting' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES PAINTING INTEREST
-	$( '#painting img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#exercising' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS EXERCISING INTEREST
+		$( '#exercising img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '100%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-// ---------- OUTDOORS ICON ---------- //
+	$( '#exercising' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES EXERCISING INTEREST
+		$( '#exercising img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-$( '#outdoors' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS OUTDOORS INTEREST
-	$( '#outdoors img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- TRAVELING ICON ---------- //
 
-$( '#outdoors' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES OUTDOORS INTEREST
-	$( '#outdoors img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '40%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#traveling' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS TRAVELING INTEREST
+		$( '#traveling img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-// ---------- SPORTS ICON ---------- //
+	$( '#traveling' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES TRAVELING INTEREST
+		$( '#traveling img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-$( '#sports' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS SPORTS INTEREST
-	$( '#sports img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- PAINTING ICON ---------- //
 
-$( '#sports' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES SPORTS INTEREST
-	$( '#sports img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	$( '#painting' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS PAINTING INTEREST
+		$( '#painting img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-// ---------- ANIMALS ICON ---------- //
+	$( '#painting' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES PAINTING INTEREST
+		$( '#painting img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
 
-$( '#animals' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS ANIMALS INTEREST
-	$( '#animals img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
-		opacity: 0.4 //ADD A 40% TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
-} );
+	// ---------- OUTDOORS ICON ---------- //
 
-$( '#animals' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES ANIMALS INTEREST
-	$( '#animals img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
-		width: '40%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
-		opacity: 1.0 //TAKE OUT THE TRANSPARENCY
-	}, 1500 ); //DURATION OF EVENT
+	$( '#outdoors' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS OUTDOORS INTEREST
+		$( '#outdoors img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
+	$( '#outdoors' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES OUTDOORS INTEREST
+		$( '#outdoors img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '40%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
+	// ---------- SPORTS ICON ---------- //
+
+	$( '#sports' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS SPORTS INTEREST
+		$( '#sports img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
+	$( '#sports' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES SPORTS INTEREST
+		$( '#sports img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '45%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
+	// ---------- ANIMALS ICON ---------- //
+
+	$( '#animals' ).mouseenter( function() { //LISTEN FOR MOUSE ENTERS ANIMALS INTEREST
+		$( '#animals img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '70%', //EXPAND THE WIDTH OF THE IMAGE TO 70% OF ITS SIZE
+			opacity: 0.4 //ADD A 40% TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
+	$( '#animals' ).mouseleave( function() { //LISTEN FOR WHEN MOUSE LEAVES ANIMALS INTEREST
+		$( '#animals img' ).animate( { //TARGET AND ANIMATE THE IMAGE IN ARTICLE
+			width: '40%', //SHRINK THE WIDTH BACK TO ITS ORIGINAL SIZE
+			opacity: 1.0 //TAKE OUT THE TRANSPARENCY
+		}, 1500 ); //DURATION OF EVENT
+	} );
+
 } );
