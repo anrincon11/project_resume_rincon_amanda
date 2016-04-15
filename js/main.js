e53acce95e67e79d5f44278bf00ecf6980969318
Amanda Rincon <anrincon@ufl.edu>
2016-04-15 15:05:02 -0700

    customize scrollUp plugin to fit project theme

--- js/main.js
+++ js/main.js
@@ -29,11 +29,11 @@ $( '#six' ).click( function() { //LISTEN FOR CLICK EVENT ON NAV LINK
 $( function() {
 	$.scrollUp( {
 		scrollName: 'scrollUp', // Element ID
-		topDistance: '300', // Distance from top before showing element (px)
-		topSpeed: 300, // Speed back to top (ms)
+		topDistance: '200', // Distance from top before showing element (px)
+		topSpeed: 800, // Speed back to top (ms)
 		animation: 'fade', // Fade, slide, none
-		animationInSpeed: 200, // Animation in speed (ms)
-		animationOutSpeed: 200, // Animation out speed (ms)
+		animationInSpeed: 500, // Animation in speed (ms)
+		animationOutSpeed: 100, // Animation out speed (ms)
 		scrollText: 'Scroll to top', // Text for element
 		activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 	} );
